566ca7312d82c40d1b3796547f1741560270d3c0
jaspen <jasper.norring@gmail.com>
2022-12-03 15:45:34 +0200

    Python koodi

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -3,6 +3,22 @@ from flask import Flask, request, Response
 
 app = Flask(__name__)
 
+def cursor(query):
+    sqlcursor = connection.sqlconnect.cursor()
+    sqlcursor.execute(query)
+
+
+def cursor_fetchall(query):
+    sqlcursor = connection.sqlconnect.cursor()
+    sqlcursor.execute(query)
+    outcome = sqlcursor.fetchall()
+    return outcome
+
+def gamestart():
+    query = f'''DELETE FROM game, goal_reached;'''
+    cursor(query)
+
+
 
 # Server start.
 if __name__ == '__main__':
