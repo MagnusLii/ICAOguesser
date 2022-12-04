5d1711f57823ebb6736886b1dc451ef2f34e4f04
mistop1 <misto.pantzar@metropolia.fi>
2022-12-04 13:14:11 +0200

    Uutuuksia

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -19,12 +19,24 @@ def cursor_fetchall(query):
 
 
 @app.route('/1')
-def gamestart():
+def cleardata():
     query = "DELETE FROM goal_reached;"
     cursor(query)
     query = "DELETE FROM game;"
     cursor(query)
     return '', 204
+
+@app.route('/2' ,methods=['POST'])
+def updatename():
+    givenName = request.form['InsertedName']
+    print(givenName)
+    i = 1
+    query = f'''INSERT INTO game (id, screen_name) VALUES ({i},"{givenName}");'''
+    cursor(query)
+
+    return '', 204
+
+
 
 # Server start.
 if __name__ == '__main__':
