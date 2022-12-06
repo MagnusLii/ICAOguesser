af2901a80781008a576d2d830413af5a911d445f
Mage <magnus.liivamagi@gmail.com>
2022-12-06 09:22:19 +0200

    Cleaned up code.

--- JS/maps.js
+++ JS/maps.js
@@ -2,15 +2,16 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     mapId: 'eedb0a8713ca32aa',
     center: {lat: 48.85, lng: 2.35},
-    zoom: 12,
+    zoom: 4,
   });
-
+  // Creates markers on the map.
   const marker = new google.maps.Marker({
     position: {lat: 60.3172, lng: 24.963301},
     map,
     title: 'Hello World!',
 
   });
+  // Creates an infowindow for the marker.
   const infowindow = new google.maps.InfoWindow({
     content: 'HELSINKI-VANTAAN LENTOKENTTÄ',
     ariaLabel: 'Uluru',
--- PYTHON/main.py
+++ PYTHON/main.py
@@ -33,18 +33,20 @@ def clearData():
 def setupGame():
     clearData()
     givenName = request.form['InsertedName']
-    print(givenName)
     i = 1
-    query = f'''INSERT INTO game (id, screen_name) VALUES ({i},"{givenName}");'''
+    query = f'''INSERT INTO game (id, screen_name)
+                VALUES ({i},"{givenName}");'''
     cursor(query)
     return '', 204
 
 
+# Fetches airport names, lat- and longitude degrees for future use in map markers.
 def search_airport():
-    query = f'SELECT airport.name, airport.latitude_deg, airport.longitude_deg FROM airport;'
-    list6 = cursor_fetchall(query)
-    list5 = []
-    for row in list6:
+    query = f'''SELECT airport.name, airport.latitude_deg, airport.longitude_deg
+                FROM airport;'''
+    results = cursor_fetchall(query)
+    json_list = []
+    for row in results:
         name = row[0]
         latitude = row[1]
         longitude = row[2]
@@ -53,13 +55,9 @@ def search_airport():
             "latitude": latitude,
             "longitude": longitude
         }
-        list5.append(json)
-    print(list5)
-
-
-
-search_airport()
-
+        json_list.append(json)
+    return json_list
+#TODO send data to JS for marker processing.
 
 
 # Server start.
