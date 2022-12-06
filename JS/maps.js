1ee0a93434083815680b7b0eab7ef7a8f279fa24
Mage <magnus.liivamagi@gmail.com>
2022-12-06 14:18:05 +0200

    Cleaned up code and added comments.

--- JS/maps.js
+++ JS/maps.js
@@ -1,34 +1,37 @@
 function initMap() {
+  let trackingnum = 0
   map = new google.maps.Map(document.getElementById('map'), {
     mapId: 'eedb0a8713ca32aa',
     center: {lat: 48.85, lng: 2.35},
     zoom: 4,
   });
-  const markers = [{"name": "Nassen airport", "latitude": 10.123, "longitude": 124.42},
+  // Temp JSON data for testing purposes, will be replaced by JSON data from backend later on.
+  const markersJSON = [{"name": "Nassen airport", "latitude": 10.123, "longitude": 124.42},
       {"name": "Jasperin airport","latitude": 50,"longitude": 30,},
       {"name": "Miston Airport","latitude": 12.341,"longitude": 1.1},
       {"name": "Danielin Airport","latitude": -8,"longitude": -56.3}]
-  for (let i = 0; i < markers.length; i++) {
-    let marker = new google.maps.Marker({
-    position: {lat: markers[i].latitude, lng: markers[i].longitude},
-    map,
-    title: 'Olet täällä Nasse!',
-  });
-  }
-  // Creates markers on the map.
+
+  for (let i = 0; i < markersJSON.length; i++) {
+
+    // Creates markers on the map.
   let marker = new google.maps.Marker({
-    position: {lat: 60.3172, lng: 24.963301},
+    position: {lat: markersJSON[i].latitude, lng: markersJSON[i].longitude},
     map,
-    title: 'Olet täällä Nasse!',
+    title: 'Airport',
   });
 
   // Creates an infowindow for the marker.
-  let trackingnum = 0
   const infowindow = new google.maps.InfoWindow({
     content: '<a class="marker-button-' + trackingnum + '"><button>Lock in this airport</button></a>',
     ariaLabel: 'Uluru',
   });
+
+  trackingnum += 1; // Updates tracking num for next airport.
+
   marker.addListener('click', () => {
     infowindow.open(map, marker);
   });
+
+  }
+
 }
